58973a85bfaf3d597c76a693e1371957c3383427
mobinnesari81 <mobinnesari81@gmail.com>
2021-10-12 13:58:10 +0330

    Initial Commit

---
+++ Processor.py
@@ -0,0 +1,21 @@
+# Project: Create a program which could compute all of the statistics parameters with some data
+# This whole project Designed and Developed by : Mobin Nesari 99222107 CopyRight T.Stark
+# Hope you enjoy it :D
+
+# Libraries:
+import numpy as np
+import matplotlib.pyplot as plt
+#   Notice that you need both numpy and matplotlib libraries for running this program.
+
+# Functions:
+
+
+
+
+
+
+
+
+
+
+# Main program:
